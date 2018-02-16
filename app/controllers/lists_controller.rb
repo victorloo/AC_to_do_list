efe28b4db34355e88ab360549305de20b935310f
victorloo <ila9970@gmail.com>
2018-02-16 20:50:14 +0800

    user can create new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,4 +3,34 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
+
+  def show
+    
+  end
+
+  def new
+    @list = List.new  
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+
+    redirect_to root_path
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :due_date, :note)
+  end
+
 end
